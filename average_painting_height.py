88ef99092415642ddf44228ab06e3681d4a2fcf8
Annie Liemohn <aliemohn@umich.edu>
2024-12-10 15:24:55 -0500

    still adding comments lol

--- average_painting_height.py
+++ average_painting_height.py
@@ -1,16 +1,19 @@
-# import python libraries needed
+# import python libraries as needed
 
 import sqlite3                      # for connecting to a database
 import matplotlib.pyplot as plt     # for plots
 import os                           # for ensuring correct file paths
 import csv                          # for writing to a csv file
 
+
+# function: connect to a database and pull necessary variables for analysis
 def get_data(database):
     path = os.path.dirname(os.path.abspath(__file__))           # establish path to database
     conn = sqlite3.connect(path + "/" + database)               # establish connection
-    cur = conn.cursor()
+    cur = conn.cursor()                                         # create cursor
 
-    cur.execute("""
+    # gather creation_year and height_cm variables from Harvard table in Museums.db database
+    cur.execute("""                                             
         SELECT creation_year, height_cm FROM Harvard
         WHERE creation_year IS NOT NULL
         AND height_cm IS NOT NULL
@@ -19,20 +22,22 @@ def get_data(database):
     conn.close()
     return data
 
-def calculate_average_height(data):
-    height_dist = {}
 
-    early_eighteen_count = 0
+# function: create dictionary with time periods of painting's creation as the year as the keys and number of paintings in that time period as the values
+def calculate_average_height(data):
+    height_dist = {}                                # create empty dictionary                        
+
+    early_eighteen_count = 0                        # establish empty count variables for total number of paintings in a given time period
     late_eighteen_count = 0
     early_nineteen_count = 0
     late_nineteen_count = 0
     early_2000s_count = 0
     
-    for creation_year, height_cm in data:
+    for creation_year, height_cm in data:           # filter painting data based on creation_year variable and sum total painting heights
         if 1800 <= creation_year <= 1851:
             if '1800-1850' not in height_dist:
-                height_dist['1800-1850']  = 0
-            height_dist['1800-1850'] += height_cm
+                height_dist['1800-1850']  = 0       # if first collected painting for a time period, create an integer placeholder
+            height_dist['1800-1850'] += height_cm   # add painting height onto the current sum for paintings in that time period
             early_eighteen_count += 1
 
         elif 1851 <= creation_year <= 1900:
@@ -59,8 +64,10 @@ def calculate_average_height(data):
             height_dist['2000-2050'] += height_cm
             early_2000s_count += 1
 
+        # create a list for total painting height sums for each time period
         count_lst = [early_eighteen_count, late_eighteen_count, early_nineteen_count, late_nineteen_count, early_2000s_count]
-
+        
+    # divide the total painting height sums for each time period by total number of paintings in that time period
     index = 0
     for key in height_dist:
         average = round((height_dist[key]) / (count_lst[index]), 2)
